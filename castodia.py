f83a95e8b75a34c1ee2223d7d2df9e2db28839b1
ZKTKZ <tshahjah@uwaterloo.ca>
2020-09-03 05:04:18 -0400

    GET resolver

--- castodia.py
+++ castodia.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request, url_for
+from markupsafe import escape
 from flask_sqlalchemy import SQLAlchemy
 
 #TO-DO: move database_uri values to ENV vars
@@ -7,11 +8,12 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:pw@ec2-18-191-192
 db = SQLAlchemy(app)
 
 class User(db.Model):
+    #TO-DO: confirm primary_key is internally checked for uniqueness
     name = db.Column(db.String(128), primary_key=True)
     email = db.Column(db.String(254), unique=True)
     age = db.Column(db.Integer)
     #TO-DO: automatic current timestamp
-    timestamp = db.Column(db.TIMESTAMP, server_default=)
+    timestamp = db.Column(db.TIMESTAMP)#, server_default=)
 
     def __repr__(self):
         return self.name
@@ -25,7 +27,17 @@ class Test1(db.Model):
     def __repr__(self):
         return  self.test
 
-#TO-DO: confirm primary_key is internally checked for uniqueness
-
 #ORM -> DB with the following
 #db.create_all()
+
+@app.route('/get_info/<name>')
+def get_info(__name):
+    user = User.query.filter_by(name=__name).first()
+    return escape('email: %s' % user.email + ', age: %s ' % user.age)
+
+#Test url string format; 
+with app.test_request_context():
+    print(url_for('get_info', name='abc'))
+
+#Test return value
+print(get_info('abc'))
